381d5ab14f0eaa20d5be6912eb8465aa9cf3e577
rafaelcirinosilva <146239342+rafaelcirinosilva@users.noreply.github.com>
2024-10-22 09:56:54 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -5,20 +5,20 @@ criaCartao(
 )
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual país tem mais títulos da Copa do Mundo?',
+    'O Brasil é o país com mais títulos, totalizando cinco conquistas.'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que significa a sigla VAR no futebol?',
+    'VAR significa "Video Assistant Referee", ou Árbitro Assistente de Vídeo, uma tecnologia usada para revisar decisões arbitrárias.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem é o maior artilheiro da história da Liga dos Campeões da UEFA?',
+    'Cristiano Ronaldo é o maior artilheiro da história da Liga dos Campeões, com mais de 130 gols.'
 )
 criaCartao(
     'Programação',
